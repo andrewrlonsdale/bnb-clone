dc16b7ba2ffb3e4401f0a234904b9a9da48b94d1
Andrew Lonsdale <andrewlonsdale@Andrews-Mac.local>
2017-10-04 13:11:20 +0100

    add bootstrap

--- AirBnb/app/helpers/application_helper.rb
+++ AirBnb/app/helpers/application_helper.rb
@@ -1,6 +1,10 @@
 module ApplicationHelper
-	def avatar_url(user)
-		gravatar_id = Digest::MD5::hexdigest(user.email).downcase
-		"https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identical&s=150"
+  def avatar_url(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    if user.image
+      "https://graph.facebook.com/#{user.uid}/picture?type=large"
+    else
+      "https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identical&s=150"
+    end
+  end
 end
-end			
